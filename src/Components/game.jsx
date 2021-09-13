84f2a7b3a1d430cb8bc31bc777a4376707f2fede
Anshika Gautam <anshikagautam130@gmail.com>
2021-09-13 18:50:51 +0530

    guess the song(play along) feature added

--- src/Components/game.jsx
+++ src/Components/game.jsx
@@ -4,13 +4,18 @@ import {
   ThemeProvider,
   createTheme,
   Typography,
+  TextField,
 } from "@material-ui/core";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useStyles } from "./gameStyle";
+import FormatQuoteRoundedIcon from "@material-ui/icons/FormatQuoteRounded";
+import Confetti from "react-confetti";
 
 export default function Game() {
   const classes = useStyles();
   const [lyrics, setLyrics] = useState("");
+  const [song, setSong] = useState("");
+  const [show, setShow] = useState(false);
 
   const theme = createTheme({
     palette: {
@@ -30,15 +35,62 @@ export default function Game() {
       });
   };
 
+  const checkAns = () => {
+    if (song.toUpperCase() === lyrics.song.toUpperCase()) {
+      setShow(true);
+      setSong("");
+      getLyrics();
+    } 
+    else {
+      setSong("");
+      alert("Wrong Guess!");
+    }
+  };
+
+  // to get the quote on reload
+  useEffect(() => {
+    getLyrics();
+  }, []);
+
   return (
     <ThemeProvider theme={theme}>
       <Container className={classes.main}>
         <h1 className={classes.head}>Hello Swifties!</h1>
-        <Typography className={classes.quote}>{lyrics.quote}</Typography>
-        <Button className={[classes.btn, '']} onClick={getLyrics}>
+        <Typography className={classes.quote}>
+          <FormatQuoteRoundedIcon className={classes.quoteIcon} />
+          <br />
+          {lyrics.quote}
+        </Typography>
+        <div className={classes.field}>
+          <TextField
+            label="Guess the song"
+            variant="filled"
+            value={song}
+            onChange={(e) => {
+              setSong(e.target.value);
+            }}
+            color="secondary"
+            fullWidth
+            style={{ width: "50%"}}
+          />
+          <Button onClick={checkAns} className={classes.checkBtn} variant="contained" type="submit">
+            Check
+          </Button>
+        </div>
+        <br />
+        <br />
+        <Button className={classes.btn} onClick={getLyrics}>
           Get Quote
         </Button>
       </Container>
+      <Confetti
+        run={show}
+        recycle={false}
+        onConfettiComplete={(confetti) => {
+          setShow(false);
+          confetti.reset();
+        }}
+      />
     </ThemeProvider>
   );
 }
--- src/Components/gameStyle.js
+++ src/Components/gameStyle.js
@@ -1,33 +1,60 @@
 import { makeStyles } from "@material-ui/core";
 
 export const useStyles = makeStyles((theme) => ({
-    main:
-    {
-        textAlign: 'center',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'column',
-        height: '100vh',
-        width: '100vw'
+  main: {
+    textAlign: "center",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column",
+    height: "100%",
+    width: "100%",
+    padding: "4rem 0rem",
+  },
+  head: {
+    fontSize: "4rem",
+    marginBottom: '2rem',
+    width: '100%',
+    textTransform: "uppercase",
+    letterSpacing: "0.5rem",
+  },
+  quote: {
+    height: "30%",
+    fontSize: "1.5rem",
+    width: '60%',
+    margin: '2rem 0rem'
+  },
+  btn: {
+    fontSize: "1.5rem",
+    padding: "0rem 3.5rem",
+    textTransform: "uppercase",
+    transition: "all 0.3s ease",
+    letterSpacing: "0.3rem",
+    position: "relative",
+    boxShadow: "0 0.2rem 0.5rem 0.2rem rgba(255, 105, 135, .3)",
+    backgroundImage: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+  },
+  field: {
+    width: "100%",
+    height: "10vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    [theme.breakpoints.down("md")]: {
+      flexDirection: "column",
     },
-    head:{
-        height: '30vh',
-        fontSize: '4rem',
-        textTransform: 'uppercase',
-        letterSpacing: '0.5rem'
-    },
-    quote:{
-        height: '20vh',
-        fontSize: '1.5rem'
-    },
-    btn:{
-        fontSize: '1.5rem',
-        padding: '0rem 3.5rem',
-        textTransform: 'uppercase',
-        transition: 'all 0.3s ease',
-        letterSpacing: '0.3rem',
-        position: 'relative',
-        backgroundImage: 'linear-gradient(315deg,#FEC260 0%, #A12568 74%)',
-    }
-}))
+    "& label.Mui-focused": {
+        color: "#F4ABC4",
+      },
+  },
+  quoteIcon:{
+      transform:'scaleX(-1)',
+      fontSize: '2rem'
+  },
+  checkBtn:{
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    color: '#fff',
+    borderRadius: '0',
+    padding: '1rem 2rem',
+  }
+}));
